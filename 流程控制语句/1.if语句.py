30e42955310f005d523b4f52730756d254ba2b2b
NOHeasenCP <easen8028@gmail.com>
2026-07-04 16:33:30 +0800

    8-update

--- 流程控制语句/1.if语句.py
+++ 流程控制语句/1.if语句.py
@@ -1,5 +1,5 @@
 ##########
-#if else语句
+#if elif else语句
 ###########
 
 n=int(input("输入一个整数："))
@@ -16,4 +16,12 @@ n=int(input("输入一个整数："))
 if  not n<10 or n>20:
     print(f"{n}不在10-20之间")
 else: print("不符合条件")
+
+n=int(input("输入一个整数："))
+if   n<10 :
+    print(f"{n}小于10")
+elif n>20:
+    print(f"{n}小于10")
+else: print(f"{n}不在10-20之间")
+
 
